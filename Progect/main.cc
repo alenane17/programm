45070ae47ba44f7e5b65e35b166a1291ca2fdb8c
nito <nito@WS811.kl9.uspu.ru>
2020-09-22 15:29:04 +0500

    Добавили функцию

--- Progect/main.cc
+++ Progect/main.cc
@@ -7,15 +7,33 @@
 #include <random>
 #include <iostream>
 #include <chrono>
+#include <sstream>
+
+	int read_int (const char *question)
+	{
+		int result;
+		std::string inp;
+		re: std::cout << question;
+		std::getline (std::cin, inp);
+		{
+			std::stringstream inps {inp};
+			inps >> result;
+			if (inps.fail()){
+				std::cout << "Некорректный ввод!" << std::endl;
+						goto re;
+			}
+		}
+		return result;
+	}
 
 int main(){
 	using clk = std::chrono::system_clock;
-	//std::random_device rd;
+	std::random_device rd;
 	std::minstd_rand rnd{unsigned(clk::now().time_since_epoch().count())};
 	std::uniform_int_distribution<> d {15,25};
 	unsigned stone_count = d(rnd);
 	//Сообщаем юзверю о кол-ве камней
-	do {std::cout << "В куче " << stone_count << " камней" << std::endl;
+	std::cout << "В куче " << stone_count << " камней" << std::endl;
 	std::cout << "Каждый ход вы можете взять из кучи 1, 2 или 3 камня. Не более и не менее." << std::endl;
 	std::cout << std::endl;
 	//Спрашиваем, сколько тянет юзверь
@@ -23,7 +41,6 @@ int main(){
 	//Уменьшаем кучу
 	//Если 0 камней, то юзверь проиграл, иначе ход компьютера
 	//Если 1 камень, то проиграл компьютер
-	} while(true);
 
 
 
